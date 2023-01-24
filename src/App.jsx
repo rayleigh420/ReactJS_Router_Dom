5e0656651710d502a3547c2be83be6ee458d47f8
Duy Le <72803076+rayleigh420@users.noreply.github.com>
2023-01-24 20:37:27 +0700

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+import { useState } from "react";
+import { Routes, Route, Link } from "react-router-dom";
+
+import "./App.css";
+import Home from "./pages/Home";
+
+function App() {
+  return (
+    <>
+      <div className="App">
+        <Routes>
+          <Route index path="/home" element={<Home />} />
+        </Routes>
+      </div>
+
+      <Link to="/home">Home</Link>
+    </>
+  );
+}
+
+export default App;
